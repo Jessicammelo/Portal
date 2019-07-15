13799c5600794846d951f036dbf231f0e8c7e6e5
Jessica Melo <jessicamariademelo@hotmail.com>
2019-07-15 17:46:52 -0300

    segundo grafico quase

--- grafico.php
+++ grafico.php
@@ -13,10 +13,20 @@ $stmt = $conexao->query('SELECT * FROM instituicao WHERE id = ' . $id . ' ORDER 
 $instituicaoSelecionada =  $stmt->fetch(PDO::FETCH_ASSOC);
 $stmt = $conexao->query('SELECT * FROM sexo WHERE instituicao = ' . $id);
 $sexo =  $stmt->fetch(PDO::FETCH_ASSOC);
-$stmt = $conexao->query('SELECT * FROM faixa_etaria WHERE instituicao = '.$id);
+$stmt = $conexao->query('SELECT * FROM faixa_etaria WHERE instituicao = ' . $id);
 $faixa_etaria = $stmt->fetch(PDO::FETCH_ASSOC);
-$stmt = $conexao->query('SELECT * FROM renda_familiar WHERE instituicao = '.$id);
+$stmt = $conexao->query('SELECT * FROM renda_familiar WHERE instituicao = ' . $id);
 $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt = $conexao->query('SELECT * FROM grupo_social ORDER BY nome ASC');
+$data2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt = $conexao->query('SELECT * FROM grupo_social WHERE id = ' . $id . ' ORDER BY nome ASC ');
+$grupoSelecionado =  $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt = $conexao->query('SELECT * FROM sexo WHERE grupo_social = ' . $id);
+$sexo2 =  $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt = $conexao->query('SELECT * FROM faixa_etaria WHERE grupo_social = ' . $id);
+$faixa_etaria2 = $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt = $conexao->query('SELECT * FROM renda_familiar WHERE grupo_social = ' . $id);
+$renda_familiar2 = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <html>
@@ -28,7 +38,7 @@ $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="assets/css/style.css?v3">
+    <link rel="stylesheet" href="assets/css/style.css?v8">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <!-- <script src="./assets/css/script.js"></script> -->
     <script>
@@ -50,12 +60,57 @@ $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
             var options = {
                 title: 'Índice de confiança nas Instituíções Brasileiras',
                 subtitle: 'Universidade Regional de Blumenau, Blumenau, 2018-2019',
-                colors: ['#005fa4'],
-                bar: [groupWidth = '40px']
+                colors: ['#005fa4']
+                
             };
 
             var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
             chart.draw(data, google.charts.Bar.convertOptions(options));
+        }
+        let dados2 = [
+            ["Resposta", "Quant de Respostas", {
+                role: "style"
+            }],
+            ["Nenhuma \nConfiança", <?php echo $grupoSelecionado["nenhuma_confianca"] ?>, "#005fa4"],
+            ["Quase Nenhuma \nConfiança", <?php echo $grupoSelecionado["quase_nunhuma_confianca"] ?>, "#005fa4"],
+            ["Alguma \nConfiança", <?php echo $grupoSelecionado["alguma_confianca"] ?>, "#005fa4"],
+            ["Muita \nConfiança", <?php echo $grupoSelecionado["muita_confianca"] ?>, "#005fa4"]
+
+        ];
+
+        google.charts.load("current", {
+            packages: ["corechart"]
+        });
+        google.charts.setOnLoadCallback(drawChart2);
+
+        function drawChart2() {
+            var data = google.visualization.arrayToDataTable(dados2);
+
+            var view = new google.visualization.DataView(data);
+            view.setColumns([0, 1,
+                {
+                    calc: "stringify",
+                    sourceColumn: 1,
+                    type: "string",
+                    role: "annotation",
+                },
+                
+                2]);
+
+            var options = {
+                title: "Índice de confiança nesses grupos sociais",
+                width: 600,
+                height: 400,
+                bar: {
+                    groupWidth: "95%"
+                },
+                legend: {
+                    position: "none"
+                },
+
+            };
+            var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
+            chart.draw(view, options);
         }
     </script>
 </head>
@@ -71,7 +126,7 @@ $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
                 </div>
                 <div class="col-6">
                     <div class="col-12">
-                        <img style="width: 150px; float:right; color:  white" src="assets/image/Ícones_Focus/Focus@6x-8.png">
+                        <img style="width: 150px; float:right; color: white" src="assets/image/Ícones_Focus/Focus@6x-8.png">
                     </div>
                 </div>
             </div>
@@ -81,17 +136,17 @@ $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
     <div style="width: 100%" class="breadcrumb">
         <div class="col-md-10 offset-1  ">
             <button type="button" class="btn btn-light font-sizeBotao font-sizeIcone">
-                <a href="index.html#"><i class="fas fa-chart-bar"> Índice de confiança
+                <a href="index.php#"><i class="fas fa-chart-bar"> Índice de confiança
                         nas Intituíções Brasileiras </i></a>
             </button>
         </div>
     </div>
     <div class="container-fluid row">
         <div class="col-10 offset-1 row">
-            <div class="col-3">
-                <h5>
+            <div class="col-3" style="text-align:center">
+                <label>
                     Instituíções
-                </h5>
+                </label>
             </div>
         </div>
         <div class="col-10 offset-1 row">
@@ -123,26 +178,27 @@ $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
             <!--terminou menu esquerdo-->
             <div class="col-9 row titulo">
                 <div class="col-12">
-                    <div id="columnchart_material" style="width: 700px; height:400px;"></div>
-                    <table class="table table-bordered">
+                    <div id="columnchart_material" style="width: 700px; height:400px; margin: auto"></div>
+                    <br>
+                    <table class="table table-bordered" style="text-align:center; font-size:12px">
                         <thead>
-                            <th colspan="2">
+                            <th class="topicos" colspan="2">
                                 Sexo
                             </th>
-                            <th colspan="5">
+                            <th class="topicos" colspan="5">
                                 Faixa etária
                             </th>
-                            <th colspan="4">
-                                Renda famíliar
+                            <th class="topicos" colspan="4">
+                                Renda famíliar (R$)
                             </th>
                         </thead>
-                        <tbody>
+                        <tbody style="background-color: white">
                             <tr>
                                 <td>
-                                    Masculino
+                                    Masc.
                                 </td>
                                 <td>
-                                    Feminino
+                                    Fem.
                                 </td>
                                 <td>
                                     16-24
@@ -171,10 +227,8 @@ $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
                                 <td>
                                     Recusou
                                 </td>
-                                
                             </tr>
                             <tr>
-
                                 <td>
                                     <?php echo $sexo['masculino'] ?>
                                 </td>
@@ -182,31 +236,31 @@ $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
                                     <?php echo $sexo['feminino'] ?>
                                 </td>
                                 <td>
-                                <?php echo $faixa_etaria['16_24'] ?>
+                                    <?php echo $faixa_etaria['16_24'] ?>
                                 </td>
                                 <td>
-                                <?php echo $faixa_etaria['25_29'] ?>
+                                    <?php echo $faixa_etaria['25_29'] ?>
                                 </td>
                                 <td>
-                                <?php echo $faixa_etaria['30_39'] ?>
+                                    <?php echo $faixa_etaria['30_39'] ?>
                                 </td>
                                 <td>
-                                <?php echo $faixa_etaria['40_49'] ?>
+                                    <?php echo $faixa_etaria['40_49'] ?>
                                 </td>
                                 <td>
-                                <?php echo $faixa_etaria['50'] ?>
+                                    <?php echo $faixa_etaria['50'] ?>
                                 </td>
                                 <td>
-                                <?php echo $renda_familiar['ate_2000'] ?>
+                                    <?php echo $renda_familiar['ate_2000'] ?>
                                 </td>
                                 <td>
-                                <?php echo $renda_familiar['2000_6000'] ?>
+                                    <?php echo $renda_familiar['2000_6000'] ?>
                                 </td>
                                 <td>
-                                <?php echo $renda_familiar['mais_6000'] ?>
+                                    <?php echo $renda_familiar['mais_6000'] ?>
                                 </td>
                                 <td>
-                                <?php echo $renda_familiar['recusou'] ?>
+                                    <?php echo $renda_familiar['recusou'] ?>
                                 </td>
                             </tr>
                         </tbody>
@@ -215,47 +269,135 @@ $renda_familiar = $stmt->fetch(PDO::FETCH_ASSOC);
             </div>
         </div>
     </div>
-    </div>
-    <!--
-    <div class="col-10 offset-1 row">
-        
-        <div class="col-12">
-            <div style="width: 100%">
-                <div id="barchart_values" style="width: 500px; height:400px; margin: auto"></div>
-                <div class=" row">
-                    <div class="col-10 offset-2 row ">
-                        <div class="col-3 ">
-                            <div class="topicos" onclick="atualizaDadosGraficos(1)">
-                                <img style="width: 30px" src="assets/image/Ícones_Focus/2 parentes@6x-8.png">
-                                <span>Seus parentes</span>
-                            </div>
-                            <div class="topicos" onclick="atualizaDadosGraficos(2)">
-                                <img style="width: 30px" src="assets/image/Ícones_Focus/2 amigos@6x-8.png">
-                                <span>Seus amigos</span>
-                            </div>
+    <!-- inicio segundo grafico-->
+    <br>
+    <br>
+    <div class="container-fluid row">
+        <div class="col-10 offset-1 row">
+            <div class="col-3" style="text-align:center">
+                <label>
+                    Grupos sociais
+                </label>
+            </div>
+        </div>
+        <div class="col-10 offset-1 row ">
+            <div class="col-3 ">
+                <?php
+                for ($i = 0; $i < count($data2); $i++) {
+                    if ($data2[$i]['id'] == $id) {
+                        ?>
+                        <div class="topicos selecionado">
+                            <img style="width: 30px" src="assets/image/Ícones_Focus/<?php echo $data2[$i]['icone'] ?>">
+                            <span class="removerLinha"><?php echo $data2[$i]['nome'] ?></span>
                         </div>
-                        <div class="col-4">
-                            <div class="topicos" onclick="atualizaDadosGraficos(3)">
-                                <img style="width: 30px" src="assets/image/Ícones_Focus/2 pessoas da familia@6x-8.png">
-                                <span>Pessoas da sua família</span>
+                    <?php
+                    } else {
+                        ?>
+                        <a class="removerLinha" href="grafico.php?id=<?php echo $data2[$i]['id'] ?>">
+                            <div class="topicos">
+                                <img style="width: 30px" src="assets/image/Ícones_Focus/<?php echo $data2[$i]['icone'] ?>">
+                                <span class="removerLinha"><?php echo $data2[$i]['nome'] ?></span>
                             </div>
-                        </div>
-                        <div class="col-3">
-                            <div class="topicos" onclick="atualizaDadosGraficos(4)">
-                                <img style="width: 30px" src="assets/image/Ícones_Focus/2 seus vizinhos@6x-8.png">
-                                <span"> Seus vizinhos</span>
-                            </div>
-                            <div class="topicos" onclick="atualizaDadosGraficos(5)">
-                                <img style="width: 30px" src="assets/image/Ícones_Focus/2 brasileiros@6x-8.png">
-                                <span>Brasileiros em Geral</span>
-                            </div>
-                        </div>
-                    </div>
+                        </a>
+                    <?php
+                    }
+                    ?>
+                <?php
+                }
+                ?>
+            </div>
+            <div class="col-9 row titulo">
+                <div class="col-12">
+                    <div id="barchart_values" style="width: 500px; height:400px; margin: auto"></div>
+                    <table class="table table-bordered" style="text-align:center; font-size:12px">
+                        <thead>
+                            <th class="topicos" colspan="2">
+                                Sexo
+                            </th>
+                            <th class="topicos" colspan="5">
+                                Faixa etária
+                            </th>
+                            <th class="topicos" colspan="4">
+                                Renda famíliar (R$)
+                            </th>
+                        </thead>
+                        <tbody style="background-color: white">
+                            <tr>
+                                <td>
+                                    Masc.
+                                </td>
+                                <td>
+                                    Fem.
+                                </td>
+                                <td>
+                                    16-24
+                                </td>
+                                <td>
+                                    25-29
+                                </td>
+                                <td>
+                                    30-39
+                                </td>
+                                <td>
+                                    40-49
+                                </td>
+                                <td>
+                                    50+
+                                </td>
+                                <td>
+                                    Até 2000
+                                </td>
+                                <td>
+                                    2000-6000
+                                </td>
+                                <td>
+                                    +6000
+                                </td>
+                                <td>
+                                    Recusou
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <?php echo $sexo2['masculino'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $sexo2['feminino'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $faixa_etaria2['16_24'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $faixa_etaria2['25_29'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $faixa_etaria2['30_39'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $faixa_etaria2['40_49'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $faixa_etaria2['50'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $renda_familiar2['ate_2000'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $renda_familiar2['2000_6000'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $renda_familiar2['mais_6000'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $renda_familiar2['recusou'] ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-            -->
     <br>
     <br>
 </body>
